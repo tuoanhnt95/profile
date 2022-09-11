06f55167c9e0f8de5cc042605611fc9d20f50574
Oanh Nguyen <myspamvn@yahoo.com>
2022-09-11 17:54:08 +0900

    feat: Add projects' images and links

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -3,11 +3,14 @@ const bannerBackground = site.querySelector(".banner-background");
 const bannerGradient = site.querySelector(".banner-gradient");
 const buttonProject = site.querySelector("#btn-project");
 const headerContent = site.querySelector(".header-content")
+const banner = site.querySelector(".banner")
+const about = site.querySelector("#about")
 
 buttonProject.addEventListener('click', (event) => {
   console.log(bannerBackground.outerHTML);
   bannerBackground.classList.toggle("banner-zoom")
   bannerGradient.classList.toggle("banner-opacity")
   headerContent.classList.toggle("content-fade")
+  about.classList.toggle("d-none")
   }
 )
